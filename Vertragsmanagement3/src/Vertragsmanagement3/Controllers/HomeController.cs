87e657e9d929a9fdd3b6bdcc9aa031219b8843f6
Daniel <Dan.Bmh@gmail.com>
2016-05-28 22:07:53 +0200

    Test

--- Vertragsmanagement3/src/Vertragsmanagement3/Controllers/HomeController.cs
+++ Vertragsmanagement3/src/Vertragsmanagement3/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace Vertragsmanagement3.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "My contact page.";
 
             return View();
         }
